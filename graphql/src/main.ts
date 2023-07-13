c14a518171c708237cd4af40df08e71c656f7724
Jesse Hazen <jhazen2020@gmail.com>
2023-07-12 21:33:28 -0600

    integrate front end

--- graphql/src/main.ts
+++ graphql/src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
+import cors = require('cors');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.use(
+    '/graphql',
+    cors({
+      origin: [
+        'http://localhost:4040',
+        'http://localhost:3000',
+        'https://studio.apollographql.com',
+      ],
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
