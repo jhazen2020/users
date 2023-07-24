a341fe2b5f132e70834889696df44426cbb4c2c5
Jesse Hazen <jhazen2020@gmail.com>
2023-07-24 13:12:12 -0600

    correct well known path

--- graphql/src/app.controller.ts
+++ graphql/src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('.wellknown/pki-validation/251D447FB92B0DBBF9C19305AE177478.txt')
+@Controller('.well-known/pki-validation/251D447FB92B0DBBF9C19305AE177478.txt')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
