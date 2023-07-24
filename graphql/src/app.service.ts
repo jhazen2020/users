e9f1accbf8a1b470b6940977fb67be39b9eae674
Jesse Hazen <jhazen2020@gmail.com>
2023-07-24 12:13:08 -0600

    add cert verification

--- graphql/src/app.service.ts
+++ graphql/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return '9B117E3CBD5E1B759D3416EA5CEA2C59BF305F262128F8EEC3DC1136925B88A8 comodoca.com 64beb80985b6a';
   }
 }
