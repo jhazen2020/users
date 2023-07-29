60ea930dd6d966eb82b11686357fbd7d15d25c0a
Jesse Hazen <jhazen2020@gmail.com>
2023-07-29 11:10:39 -0600

    chore: add exception handling to

--- graphql/src/users/fake-users.service.ts
+++ graphql/src/users/fake-users.service.ts
@@ -1,17 +1,17 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { Users } from './users.entity';
-import { ExceptionHandler } from 'lib/exception-handler';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Cache } from 'cache-manager';
+import { ExceptionHandler } from 'lib/exception-handler';
 
 @Injectable()
 export class FakeUsersService {
   constructor(
-    @Inject(ExceptionHandler)
-    private excpetionHandler: ExceptionHandler,
     @Inject(CACHE_MANAGER) private cacheManager: Cache,
+    @Inject(ExceptionHandler)
+    private exceptionHandler: ExceptionHandler,
   ) {}
-  async getFakeUsersAscendingOrder(
+  private async getFakeUsersAscendingOrder(
     startingIndex: number,
     endIndex: number
   ): Promise<Map<number,  Users>> {
@@ -23,7 +23,7 @@ export class FakeUsersService {
     }
     return users;
   }
-  async getFakeUsersDescendingOrder(
+  private async getFakeUsersDescendingOrder(
     startingIndex: number,
     endIndex: number,
   ): Promise<Map<number, Users>> {
@@ -36,7 +36,7 @@ export class FakeUsersService {
     if(typeof users === 'undefined') throw new Error('No cache Data.');
     return users;
   }
-  setCurrentUserInFakeUsersList(
+  private setCurrentUserInFakeUsersList(
     usersList: Map<number, any>,
     currentUser: Record<string, any>,
   ) {
@@ -48,17 +48,17 @@ export class FakeUsersService {
     
     return usersList;
   }
-  getStartingIndex(page: number, pageSize: number) {
+  private getStartingIndex(page: number, pageSize: number) {
     if (page < 1) {
       throw new Error('page must be above zero');
     }
     return (page - 1) * pageSize + 1;
   }
-  getEndIndex(page: number, pageSize: number, count: number) {
+  private getEndIndex(page: number, pageSize: number, count: number) {
     const total = page * pageSize;
     return total > count ? count : page * pageSize;
   }
-  getStartingIndexDesc(page: number, pageSize: number, count: number) {
+  private getStartingIndexDesc(page: number, pageSize: number, count: number) {
     if (page < 1) {
       throw new Error('page must be above zero');
     }
@@ -70,14 +70,14 @@ export class FakeUsersService {
     }
     return inversePage * pageSize > count ? count : inversePage * pageSize - diff;
   }
-  getEndIndexDesc(page: number, pageSize: number, count: number) {
+  private getEndIndexDesc(page: number, pageSize: number, count: number) {
     const pages = Math.ceil(count / pageSize);
     if(page === pages){
         return 1;
     }
     return (count - (page * pageSize))  + 1;
   }
-  validatePage(page: number, pageSize: number, count: number) {
+  private validatePage(page: number, pageSize: number, count: number) {
     if (page < 1) {
       throw new Error('page must be above zero');
     }
@@ -94,23 +94,28 @@ export class FakeUsersService {
     order: 'asc' | 'desc' | 'ASC' | 'DESC',
     currentUser: Record<string, any>,
   ) {
-    let usersList;
-    if (currentUser === null) throw new Error('no user found');
-    this.validatePage(page, pageSize, count);
-
-    if (order === 'asc' || order === 'ASC') {
-      const startingIndex = this.getStartingIndex(page, pageSize);
-      const endIndex = this.getEndIndex(page, pageSize, count);
-      
-      usersList = await this.getFakeUsersAscendingOrder(
-        startingIndex,
-        endIndex
-      );
-    } else {
-      const startIndex = this.getStartingIndexDesc(page, pageSize, count);
-      const endIndex = this.getEndIndexDesc(page, pageSize, count);
-      usersList = await this.getFakeUsersDescendingOrder(startIndex, endIndex);
+    try{
+      let usersList;
+      if (currentUser === null) throw new Error('no user found');
+      this.validatePage(page, pageSize, count);
+  
+      if (order === 'asc' || order === 'ASC') {
+        const startingIndex = this.getStartingIndex(page, pageSize);
+        const endIndex = this.getEndIndex(page, pageSize, count);
+        
+        usersList = await this.getFakeUsersAscendingOrder(
+          startingIndex,
+          endIndex
+        );
+      } else {
+        const startIndex = this.getStartingIndexDesc(page, pageSize, count);
+        const endIndex = this.getEndIndexDesc(page, pageSize, count);
+        usersList = await this.getFakeUsersDescendingOrder(startIndex, endIndex);
+      }
+      return this.setCurrentUserInFakeUsersList(usersList, currentUser);
+    }catch(e){
+      this.exceptionHandler.graphqlErrorHandler('Unable to get the modified users list.', e);
     }
-    return this.setCurrentUserInFakeUsersList(usersList, currentUser);
+    
   }
 }
--- graphql/src/users/users.service.ts
+++ graphql/src/users/users.service.ts
@@ -12,70 +12,100 @@ export class UsersService {
     @InjectRepository(Users)
     private usersRepository: Repository<Users>,
     @Inject(ExceptionHandler)
-    private excpetionHandler: ExceptionHandler,
+    private exceptionHandler: ExceptionHandler,
   ) {}
   async addUser(user: UsersInput) {
     try {
       return await this.usersRepository.insert(user);
     } catch (e) {
-      this.excpetionHandler.graphqlErrorHandler(e);
+      this.exceptionHandler.graphqlErrorHandler('Unable to add user', e);
     }
   }
   async updateUser(user: Omit<usersModel, 'user_category' | 'id'>) {
-    return await this.usersRepository.update({ email: user.email }, user);
+    try {
+      return await this.usersRepository.update({ email: user.email }, user);
+    } catch (e) {
+      this.exceptionHandler.graphqlErrorHandler('Unable to update user', e);
+    }
   }
   async getLoggedInUserWithFakeUsers(
     limit: number = 10,
     cursor: number | undefined = 0,
     order: 'desc' | 'asc' | 'DESC' | 'ASC' = 'DESC',
   ) {
-    return await this.usersRepository.find({
-      relations: ['usersCategories'],
-      where: {
-        id: MoreThanOrEqual(cursor),
-      },
-      take: limit,
-      order: {
-        id: {
-          direction: order,
+    try {
+      return await this.usersRepository.find({
+        relations: ['usersCategories'],
+        where: {
+          id: MoreThanOrEqual(cursor),
         },
-      },
-    });
+        take: limit,
+        order: {
+          id: {
+            direction: order,
+          },
+        },
+      });
+    } catch (e) {
+      this.exceptionHandler.graphqlErrorHandler('Unable to get users list', e);
+    }
   }
   async getUsers(
     limit: number = 10,
     cursor: number | undefined = 0,
     order: 'desc' | 'asc' | 'DESC' | 'ASC' = 'DESC',
   ) {
-    return await this.usersRepository.find({
-      relations: ['usersCategories'],
-      where: {
-        id: MoreThanOrEqual(cursor),
-      },
-      take: limit,
-      order: {
-        id: {
-          direction: order,
+    try {
+      return await this.usersRepository.find({
+        relations: ['usersCategories'],
+        where: {
+          id: MoreThanOrEqual(cursor),
         },
-      },
-    });
+        take: limit,
+        order: {
+          id: {
+            direction: order,
+          },
+        },
+      });
+    } catch (e) {
+      this.exceptionHandler.graphqlErrorHandler('Unable to get users list.', e);
+    }
   }
   async getCountUsers() {
-    return await this.usersRepository.count();
+    try {
+      return await this.usersRepository.count();
+    } catch (e) {
+      this.exceptionHandler.graphqlErrorHandler(
+        'Unable to get users count.',
+        e,
+      );
+    }
   }
   async getUser(email: string) {
-    const data = await this.usersRepository.findOne({
-      relations: ['usersCategories'],
-      where: {
-        email,
-      },
-    });
-    if(!data){
-      return data;
+    try {
+      const data = await this.usersRepository.findOne({
+        relations: ['usersCategories'],
+        where: {
+          email,
+        },
+      });
+      if (!data) {
+        return data;
+      }
+      return instanceToPlain(data);
+    } catch (e) {
+      this.exceptionHandler.graphqlErrorHandler('Unable to get user.', e);
     }
-    return instanceToPlain(data)
   }
   async deleteUser(id: number) {
-    return await this.usersRepository.delete(id);
+    try {
+      return await this.usersRepository.delete(id);
+    } catch (e) {
+      this.exceptionHandler.graphqlErrorHandler(
+        'Unable to delete the user.',
+        e,
+      );
+    }
   }
 }
